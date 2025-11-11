8518eb3441e6bd4f9ef1f0a94b2a463f81feac1b
matheus23 <matheus23@users.noreply.github.com>
2025-11-11 17:13:31 +0000

    deploy: d80bc051c53c660e2e15d1eeea058e4ac601ee69

--- pr/32/docs/n0_watcher/sidebar-items.js
+++ pr/32/docs/n0_watcher/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["Combine","Combine3","Direct","Disconnected","InitializedFut","Join","Map","NextFut","Stream","Watchable"],"trait":["Nullable","Watcher"]};
+window.SIDEBAR_ITEMS = {"struct":["Direct","Disconnected","InitializedFut","Join","Map","NextFut","Or","Or3","Stream","Watchable"],"trait":["Nullable","Watcher"]};
