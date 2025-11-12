61dc78133069527942ee998ece11efc9764d5bbb
matheus23 <matheus23@users.noreply.github.com>
2025-11-12 16:32:47 +0000

    deploy: 2934f6c6e0f1a9b26fad4a75e2bc5224cf9f154e

---
+++ pr/33/docs/trait.impl/core/ops/drop/trait.Drop.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["n0_watcher",[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"n0_watcher/struct.Watchable.html\" title=\"struct n0_watcher::Watchable\">Watchable</a>&lt;T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[301]}
