0227d8b9a5eab55fba661a773582812c212d3722
Frando <Frando@users.noreply.github.com>
2026-04-17 11:56:15 +0000

    deploy: 994b7dcd69c3f438a8828384bc5fb7d78080d416

---
+++ pr/53/docs/trait.impl/core/default/trait.Default.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["n0_watcher",[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"n0_watcher/struct.Watchable.html\" title=\"struct n0_watcher::Watchable\">Watchable</a>&lt;T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[453]}
