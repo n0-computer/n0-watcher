304bc6c930ff88c096594ccb3180bb5a2176633a
dignifiedquire <dignifiedquire@users.noreply.github.com>
2026-06-15 08:56:41 +0000

    deploy: 9eb8340201b081c8465881531ac97bc539f0c40a

---
+++ pr/59/docs/src-files.js
@@ -0,0 +1,2 @@
+createSrcSidebar('[["n0_watcher",["",[],["lib.rs"]]]]');
+//{"start":19,"fragment_lengths":[33]}
---
+++ pr/59/docs/trait.impl/core/fmt/trait.Display.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["n0_watcher",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"n0_watcher/struct.Disconnected.html\" title=\"struct n0_watcher::Disconnected\">Disconnected</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[290]}
